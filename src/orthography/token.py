3e1ca574bc1ccafa80967b3b9e4d9ad9ee598192
Kais Dukes <dukeskais79@gmail.com>
2023-06-23 06:38:18 +0100

    Fix type hint + initialization.

--- src/orthography/token.py
+++ src/orthography/token.py
@@ -8,7 +8,7 @@ class Token:
 
     def __init__(self, location: Location):
         self.location = location
-        self.segments: List[Segment]
+        self.segments: List[Segment] | None = None
 
     def segment(self, segment_number: int):
         return self.segments[segment_number - 1]
